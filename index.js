655d1a2d79fe7dff04efb4a2ab69abca7df9ade5
Misha <73886737+misha244@users.noreply.github.com>
2021-05-30 13:45:16 +0100

    add connection

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const cTable = require('console.table');
+
+const connection = = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'secret',
+  database : 'company_db'
+});
+
+const init = async () => {
+  await
+}
+
+connection.connect()
+init();
