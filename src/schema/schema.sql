32883acff77f8af301a4ea4bc9a823b582469f46
Misha <73886737+misha244@users.noreply.github.com>
2021-05-30 12:23:12 +0100

    edit schema.sql as given syntax error

--- src/schema/schema.sql
+++ src/schema/schema.sql
@@ -3,31 +3,28 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8 , 2 ),
     department_id INT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id)
-        REFERENCES department (id) ON DELETE CASCADE
+    FOREIGN KEY (department_id) REFERENCES departments (id) 
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id)
-        REFERENCES role (id) ON DELETE CASCADE,
-    CONSTRAINT fk_employee FOREIGN KEY (manager_id)
-        REFERENCES employee (id) ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employees (id)
 );
