48d1952418d8dca527723d807297c868e708c54d
Misha <73886737+misha244@users.noreply.github.com>
2021-06-06 19:11:16 +0100

    revert changes as not working

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
 const inquirer = require("inquirer");
+const mysql = require("mysql");
 const DB = require("./src/db/DB");
 const db = new DB("company_db");
 
-const initialQ = async () => {
-  const firstQuestion = [
-    {
-      type: "list",
-      name: "firstQ",
-      choices: [
-        "View All Employees",
-        "View All Roles",
-        "View All Departments",
-        "Add an Employee",
-        "Update Employee Role",
-        "Add a Role",
-        "Add a Department",
-        "Exit",
-      ],
-      message: "Please, select what would you like to do:",
-    },
-  ];
+const init = async () => {
+  await db.start();
 
-  const { firstQ } = await inquirer.prompt(firstQuestion);
+  const initialQ = {
+    name: "firstQ",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Roles",
+      "View All Departments",
+      "Add an Employee",
+      "Update Employee Role",
+      "Add a Role",
+      "Add a Department",
+      "Exit",
+    ],
+  };
 
-  if (firstQ === "View all departments") {
-    viewAllDepartments();
-  } else if (firstQ === "View all roles") {
-    viewAllRoles();
-  } else if (firstQ === "View all employees") {
-    viewAllEmployees();
-  } else if (firstQ === "Add new department") {
-    await addDepartment();
+  const { firstQ } = await inquirer.prompt(initialQ);
+
+  if (firstQ === "View All Employees") {
+    await viewAllEmployees();
+  } else if (firstQ === "View All Roles") {
+    await viewAllRoles();
+  } else if (firstQ === "View All Departments") {
+    await viewAllDepartments();
+  } else if (firstQ === "Add an Employee") {
+    await addEmployee();
   } else if (firstQ === "Update Employee Role") {
     await updateEmployeeRole();
-  } else if (firstQ === "Add new role") {
+  } else if (firstQ === "Add a Role") {
     await addRole();
-  } else if (firstQ === "Add new employee") {
-    await addEmployee();
-  } else if (firstQ === "Exit") {
-    await db.end();
+  } else if (firstQ === "Add a Department") {
+    await addDepartment();
+  } else {
+    if (firstQ === "Exit") {
+      await db.end();
+    }
   }
 };
+
 // initial functions
 const viewAllEmployees = async () => {
-  const employeesQuery = await db.query("SELECT * FROM employees");
-  console.table(employeesQuery);
+  let query = `SELECT first_name, last_name, title, salary, name FROM employees LEFT JOIN roles ON employees.role_id=roles.id LEFT JOIN departments ON roles.department_id=departments.id;`;
+  let data = await db.query(query);
+  console.table(data);
 };
 
 const viewAllRoles = async () => {
-  const query = "SELECT * FROM roles";
-  const data = await db.query(query);
+  let query = `SELECT * FROM roles`;
+  let data = await db.query(query);
   console.table(data);
 };
 
 const viewAllDepartments = async () => {
-  const query = "SELECT * FROM departments";
-  const data = await db.query(query);
+  let query = `SELECT * FROM departments`;
+  let data = await db.query(query);
   console.table(data);
 };
 
@@ -83,7 +88,6 @@ const addEmployee = async () => {
     last_name: newEmployee.lName,
     role_id: newEmployee.id,
   });
-  console.log(result);
   console.log(
     `${newEmployee.fName} ${newEmployee.lName} has successfully been added as an employee!`
   );
@@ -127,24 +131,22 @@ const updateEmployeeRole = async () => {
     return roles.title === updateEmployeeA.roleName;
   });
 
-  await db.parameterisedQuery(
-    "UPDATE employees SET role_id = ? WHERE id = ?;",
-    [selectedEmployee.id, selectedRole.id]
-  );
+  await db.parameterisedQuery("UPDATE roles SET title = ? WHERE id = ?;", [
+    selectedEmployee.id,
+    selectedRole.id,
+  ]);
 };
+
 const addRole = async () => {
-  const query = `SELECT * FROM employees`;
-  const data = await db.query(query);
+  let query = `SELECT * FROM employees`;
+  let data = await db.query(query);
   console.table(data);
 };
 
 const addDepartment = async () => {
-  const query = `SELECT * FROM employees`;
-  const data = await db.query(query);
+  let query = `SELECT * FROM employees`;
+  let data = await db.query(query);
   console.table(data);
 };
-const init = async () => {
-  await db.start();
-  await initialQ();
-};
+
 init();
